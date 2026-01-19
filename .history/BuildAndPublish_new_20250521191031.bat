830a25f20f036f9eb0053978f5d34e443a820e2e
vanitoo <56897888+vanitoo@users.noreply.github.com>
2026-01-19 16:29:29 +0300

    fix

--- .history/BuildAndPublish_new_20250521191031.bat
+++ .history/BuildAndPublish_new_20250521191031.bat
@@ -1,14 +1,14 @@
 rem @ECHO OFF
 SETLOCAL
 
-SET VERSION=v8.9
+SET VERSION=v8.5
 SET REGISTRY=192.168.12.104:5050
 
 ECHO === Build ===
-docker build -t file_watcher:%VERSION%   -f Dockerfile-file_watcher .
-docker build -t pdf_processor:%VERSION%  -f Dockerfile-pdf_processor .
-docker build -t xslt_processor:%VERSION% -f Dockerfile-xslt_processor .
-docker build -t api_processor:%VERSION%  -f Dockerfile-api_processor .
+#docker build -t file_watcher:%VERSION%   -f Dockerfile-file_watcher .
+#docker build -t pdf_processor:%VERSION%  -f Dockerfile-pdf_processor .
+#docker build -t xslt_processor:%VERSION% -f Dockerfile-xslt_processor .
+#docker build -t api_processor:%VERSION%  -f Dockerfile-api_processor .
 
 ECHO === Tag VERSION ===
 docker tag file_watcher:%VERSION% %REGISTRY%/file_watcher:%VERSION%
--- .history/BuildAndPublish_new_20260119144747.bat
+++ .history/BuildAndPublish_new_20260119144747.bat
@@ -5,10 +5,10 @@ SET VERSION=v8.9
 SET REGISTRY=192.168.12.104:5050
 
 ECHO === Build ===
-docker build -t file_watcher:%VERSION%   -f Dockerfile-file_watcher .
-docker build -t pdf_processor:%VERSION%  -f Dockerfile-pdf_processor .
-docker build -t xslt_processor:%VERSION% -f Dockerfile-xslt_processor .
-docker build -t api_processor:%VERSION%  -f Dockerfile-api_processor .
+#docker build -t file_watcher:%VERSION%   -f Dockerfile-file_watcher .
+#docker build -t pdf_processor:%VERSION%  -f Dockerfile-pdf_processor .
+#docker build -t xslt_processor:%VERSION% -f Dockerfile-xslt_processor .
+#docker build -t api_processor:%VERSION%  -f Dockerfile-api_processor .
 
 ECHO === Tag VERSION ===
 docker tag file_watcher:%VERSION% %REGISTRY%/file_watcher:%VERSION%
